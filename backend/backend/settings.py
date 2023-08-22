3a7fd0a278a01a20fb1ce1ed8e8a1a788ab516c1
SAADi <93855880+Saad-ki-git@users.noreply.github.com>
2023-08-22 14:52:47 +0500

    Update settings.py

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -163,8 +163,8 @@ AUTH_PASSWORD_VALIDATORS = [
 EMAIL_BACKEND="django.core.mail.backends.smtp.EmailBackend"
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
-EMAIL_HOST_USER = "copyhaitu420@gmail.com"
-EMAIL_HOST_PASSWORD = "eicdcwdgewlkuwbk"
+EMAIL_HOST_USER = "emailhere"
+EMAIL_HOST_PASSWORD = "passwordhere"
 EMAIL_USE_TLS = True
 
 # JWT Settings
